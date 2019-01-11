d232a101bdf5b89629838d743674ac0a04478287
Paul Chang <paul@Pauls-MacBook-Pro.local>
2019-01-11 12:03:04 -0800

    removed config.js keys

--- api/config.js
+++ api/config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  allowedOrigin: ["http://localhost:3000", "http://localhost:3001"],
-  mandrill: {
-    key: 'Y1xdlViAVFhaFUu5XA5evA',
-    fromAddress: 'museum@rennie.com',
-    subject: 'Rennie Museum Booking'
-  },
-  mailchimp: {
-    key: '7e4639a737b8f47ff585ded31f622d6a-us11',
-    listId: '112ed28b88'
-  },
-  cancelLink: 'http://localhost:3001/event/cancel'
+
 }
